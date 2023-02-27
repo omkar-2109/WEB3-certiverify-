8bb42d2e71e3c7503d52e1a33298ba60c7e041ed
Chandgude Nishant <99669925+ChandgudeNishant@users.noreply.github.com>
2023-02-27 11:37:57 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 
 const API_URL = "https://polygon-mumbai.g.alchemy.com/v2/PshG4gkaT0l-1bGe6eNcMSKWtxKuJ_mv";
-const PRIVATE_KEY = "ae2e5855bfe5cf2cf575e7a39127a0a10ef682fbbde14d50209c5dd0bd7be417"
+const PRIVATE_KEY = "-private-key-"
 const PUBLIC_KEY = "0x58aA76C20fFcA2e38Cb7ed6fCfa5cdEeb20b7E63";
 
 module.exports = {
